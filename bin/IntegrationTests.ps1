8f776895a45e930198d003618c51c5e3c9e2dfa9
Aaron Rinn <arinn1250@gmail.com>
2019-11-23 12:30:15 -0800

    adding in pipeline scripts

---
+++ bin/IntegrationTests.ps1
@@ -0,0 +1,36 @@
+﻿param(
+    [String]$username = "",
+    [String]$password = "",
+    [String]$catalog = "",
+    [String]$source = "",
+    [String]$secret_var = "",
+    [String]$Configuration="Debug",
+    [String]$DebugPreference="SilentlyContinue"
+);
+
+Write-Host "Secret Variable: $secret_var"
+Write-Host "Secret Variable is hunter2: "($secret_var -eq "hunter2")
+
+Push-Location src\test\integration
+
+$Env:db_username = $username;
+$Env:db_password = $password;
+$Env:db_catalog = $catalog;
+$Env:db_source = $source;
+
+$testProjects = Get-ChildItem -Recurse *.csproj
+
+foreach ($project in $testProjects) {
+    dotnet restore $project
+    dotnet clean -c $Configuration $project
+    dotnet build -c $Configuration $project
+    dotnet test -c $Configuration --no-build $project
+    
+    if (-Not $?) {
+        Pop-Location
+        throw "Test run failed. See earlier for errors"
+    }
+}
+
+
+Pop-Location
---
+++ bin/pickles.ps1
@@ -0,0 +1,21 @@
+﻿$pickles = "$Env:home\.nuget\packages\pickles.commandline\2.20.1\tools\pickles.exe"
+$features = "--feature-directory=$Env:home\RiderProjects\Evo\src\Evo.WebApi.Tests.Integration\Features"
+$reportDir = "--output-directory=$Env:home\RiderProjects\Evo\Reports\"
+$testFormat = "--test-results-format=mstest"
+$documentFormat = "--documentation-format=dhtml"
+$testResults = "--link-results-file=$Env:home\RiderProjects\Evo\src\Evo.WebApi.Tests.Integration\TestResults\*.trx"
+$projectName = "--system-under-test-name=Evo"
+
+Get-ChildItem "$Env:home\RiderProjects\Evo\src\Evo.WebApi.Tests.Integration\TestResults\*" | Remove-Item
+
+dotnet clean -c Debug
+
+dotnet test --logger:trx
+
+. $pickles `
+    $projectName `
+    $features `
+    $reportDir `
+    $testFormat `
+    $documentFormat `
+    $testResults
