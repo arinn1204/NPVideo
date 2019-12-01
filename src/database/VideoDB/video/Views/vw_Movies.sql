aab0a6e13d14d4c0b96ff851b7dee21704b16fe5
Aaron Rinn <arinn1250@gmail.com>
2019-12-01 00:08:43 -0800

    always dirty reading in views

--- src/database/VideoDB/video/Views/vw_Movies.sql
+++ src/database/VideoDB/video/Views/vw_Movies.sql
@@ -5,20 +5,19 @@
 			,p.first_name, p.middle_name, p.last_name, p.suffix
 			,roles.role_name AS 'person_role'
 			,r.source AS 'rating_source', r.value AS 'rating_value'
-		FROM video.videos v
-			WITH (NOLOCK)
-		JOIN video.ratings r
+		FROM video.videos v WITH (NOLOCK)
+		JOIN video.ratings r WITH (NOLOCK)
 			ON r.video_id = v.video_id
-		JOIN video.genre_videos gv
+		JOIN video.genre_videos gv WITH (NOLOCK)
 			ON gv.video_id = v.video_id
-		JOIN video.person_videos pv
+		JOIN video.person_videos pv WITH (NOLOCK)
 			ON pv.video_id = v.video_id
-		JOIN video.genres g
+		JOIN video.genres g WITH (NOLOCK)
 			ON g.genre_id = gv.genre_id
-		JOIN video.persons p
+		JOIN video.persons p WITH (NOLOCK)
 			ON p.person_id = pv.person_id
-		JOIN video.person_roles pr
+		JOIN video.person_roles pr WITH (NOLOCK)
 			ON pr.person_id = p.person_id
-		JOIN video.roles roles
+		JOIN video.roles roles WITH (NOLOCK)
 			ON roles.role_id = pr.role_id
 		WHERE v.video_type = 'movie';
