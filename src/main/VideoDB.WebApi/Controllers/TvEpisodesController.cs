433b2a9f4ee50e10a8834f4f902ac073d7d6107c
Aaron Rinn <arinn1250@gmail.com>
2019-11-30 23:24:26 -0800

    added TvEpisode controller

--- src/main/VideoDB.WebApi/Controllers/TvEpisodesController.cs
+++ src/main/VideoDB.WebApi/Controllers/TvEpisodesController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Evo.WebApi.Models.Requests;
+using Evo.WebApi.Models.ViewModels;
+using Evo.WebApi.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Evo.WebApi.Controllers
 {
@@ -12,5 +16,36 @@ namespace Evo.WebApi.Controllers
     [Produces("application/json")]
     public class TvEpisodesController : Controller
     {
+        private readonly IVideoService _service;
+
+        public TvEpisodesController(IVideoService service)
+        {
+            _service = service;
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+        public IActionResult UpsertTvEpisode([FromBody] TvEpisodeRequest request)
+        {
+            TvEpisodeViewModel result;
+            try
+            {
+                result = _service.UpsertTvEpisode(request);
+            }
+            catch (Exception e)
+            {
+                var response = new ErrorResponse
+                {
+                    Error = JsonConvert.SerializeObject(e)
+                };
+
+                return StatusCode(500, response);
+            }
+
+            return result.Episode.IsUpdated
+                ? NoContent() as IActionResult
+                : Created($"/videos/tvEpisodes/{request.TvEpisodeId}", result) as IActionResult;
+        }
     }
 }
---
+++ src/test/unit/VideoDB.WebApi.Tests/ControllerTests/TvEpisodeControllerTests.cs
@@ -0,0 +1,105 @@
+﻿using Evo.WebApi.Controllers;
+using Evo.WebApi.Models.Requests;
+using Evo.WebApi.Models.ViewModels;
+using Evo.WebApi.Services.Interfaces;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoDB.WebApi.Models.ViewModels;
+
+namespace VideoDB.WebApi.Tests.ControllerTests
+{
+    [TestFixture]
+    class TvEpisodeControllerTests
+    {
+        private Mock<IVideoService> _service;
+        private TvEpisodesController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new Mock<IVideoService>();
+
+            _controller = new TvEpisodesController(_service.Object);
+        }
+
+
+        [Test]
+        public void ShouldReturnCreatedWhenSuccessfulCreation()
+        {
+            var request = new TvEpisodeRequest()
+            {
+                VideoId = "tt1234",
+                TvEpisodeId = "tt1233"
+            };
+
+            _service.Setup(s => s.UpsertTvEpisode(request))
+                .Returns(new TvEpisodeViewModel
+                {
+                    Series = new SeriesViewModel
+                    {
+                        VideoId = "tt1234" 
+                    },
+                    Episode = new TvEpisode
+                    {
+                        VideoId = "tt1233"
+                    }
+                });
+
+            var result = _controller.UpsertTvEpisode(request) as CreatedResult;
+
+            result.Should().NotBe(null);
+            result.Location.Should().Be("/videos/tvEpisodes/tt1233");
+            (result.Value as TvEpisodeViewModel).Series.VideoId.Should().Be("tt1234");
+            (result.Value as TvEpisodeViewModel).Episode.VideoId.Should().Be("tt1233");
+        }
+
+        [Test]
+        public void ShouldReturnInternalServerErrorWhenAnyExceptionThrown()
+        {
+            _service.Setup(s => s.UpsertTvEpisode(It.IsAny<TvEpisodeRequest>()))
+                .Throws(new Exception());
+
+            var objectResult = _controller.UpsertTvEpisode(new TvEpisodeRequest()) as ObjectResult;
+            var videoResult = objectResult.Value as ErrorResponse;
+            Assert.That(videoResult, Is.Not.Null);
+            Assert.That(objectResult.StatusCode, Is.EqualTo(StatusCodes.Status500InternalServerError));
+            Assert.That(videoResult.Error, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldReturnNoContentWhenSuccessfulUpdate()
+        {
+            var request = new TvEpisodeRequest()
+            {
+                VideoId = "tt1234",
+                TvEpisodeId = "tt1233"
+            };
+
+            _service.Setup(s => s.UpsertTvEpisode(request))
+                .Returns(new TvEpisodeViewModel
+                {
+                    Series = new SeriesViewModel
+                    {
+                        VideoId = "tt1234",
+                        IsUpdated = true
+                    },
+                    Episode = new TvEpisode
+                    {
+                        VideoId = "tt1234",
+                        IsUpdated = true
+                    }
+                });
+
+            var result = _controller.UpsertTvEpisode(request) as NoContentResult;
+            result.Should().NotBeNull();
+
+        }
+
+    }
+}
