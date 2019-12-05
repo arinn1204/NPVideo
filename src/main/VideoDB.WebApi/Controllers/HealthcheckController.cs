1a4f6ec0dc58ec11f549a161b8658f3f86bd48ec
Aaron Rinn <arinn1250@gmail.com>
2019-12-04 16:56:28 -0800

    added healthcheck controller

---
+++ src/main/VideoDB.WebApi/Controllers/HealthcheckController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoDB.WebApi.Controllers
+{
+    public class HealthcheckController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Healthcheck()
+        {
+            return Ok(new
+            {
+                AddedVariable = Environment.GetEnvironmentVariable("SampleApplicationVariable")
+            });
+        }
+    }
+}
