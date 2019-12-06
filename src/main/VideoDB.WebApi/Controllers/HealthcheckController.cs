651af1948262d3e852e05c8dec797a09fcc9a609
Aaron Rinn <arinn1250@gmail.com>
2019-12-05 19:41:47 -0800

    added db info into healthcheck

--- src/main/VideoDB.WebApi/Controllers/HealthcheckController.cs
+++ src/main/VideoDB.WebApi/Controllers/HealthcheckController.cs
@@ -1,21 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VideoDB.WebApi.Extensions;
 
 namespace VideoDB.WebApi.Controllers
 {
     [Route("/[controller]")]
     public class HealthcheckController : BaseController
     {
+        private readonly IConfiguration _configuration;
+
+        public HealthcheckController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public IActionResult Healthcheck()
         {
+            var information = GetSqlInformation();
+
             return Ok(new
             {
-                AddedVariable = Environment.GetEnvironmentVariable("SampleApplicationVariable")
+                AddedVariable = Environment.GetEnvironmentVariable("SampleApplicationVariable"),
+                DbName = information.First(),
+                DbVersion = information.Skip(1).First()
             });
+        }
+
+        private IEnumerable<string> GetSqlInformation()
+        {
+            var information = Enumerable.Empty<string>();
+
+            var connectionString = _configuration.CreateConnectionString();
+            using var connection = new SqlConnection(connectionString);
+            using var sqlCmd = new SqlCommand("SELECT DB_NAME(), @@VERSION", connection);
+
+            sqlCmd.Connection.Open();
+            var reader = sqlCmd.ExecuteReader();
+            reader.Read();
+            information = information.Append(reader.GetString(0));
+            information = information.Append(reader.GetString(1));
+
+            return information;
         }
     }
 }
